444908407e78723a29d8e6da93e2eb1925917935
master <rudolifrudolif@gmail.com>
2020-06-22 16:21:57 +0300

    access page

--- app/js/common.js
+++ app/js/common.js
@@ -35,5 +35,18 @@ $(function() {
     });
   });
 
+//---------------------------js-----------------------
+  $('.tabs__wrap').hide();
+  $('.tabs__wrap:first').show();
+  $('.tabs ul a:first').addClass('active');
+   $('.tabs ul a').click(function(event){
+    event.preventDefault();
+    $('.tabs ul a').removeClass('active');
+    $(this).addClass('active');
+    $('.tabs__wrap').hide();
+     var selectTab = $(this).attr('href');
+    $(selectTab).fadeIn();
+  });
+
 });
 
